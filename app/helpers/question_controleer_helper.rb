912ba2834445cdf0cd2bca671635837871b9403d
Anais Arrondeau <anais.arrondeau@gmail.com>
2020-08-10 15:11:49 +0200

    exo 2 done

---
+++ app/helpers/question_controleer_helper.rb
@@ -0,0 +1,2 @@
+module QuestionControleerHelper
+end
---
+++ test/controllers/question_controleer_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class QuestionControleerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
